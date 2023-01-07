5c98ff1dde8fd9eb99fa41136d27482da5cd6f3f
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2023-01-07 23:21:05 +0100

    add class about

--- mbsite/models.py
+++ mbsite/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Mysilf(models.Model):
+    name = models.CharField(max_length=25)
+    age = models.CharField(max_length=50)
+
+
+class About(models.Model):
+    image=models.ImageField(upload_to='mbsite/')
+    headding=models.CharField(max_length=300)
+    description=models.CharField(max_length=300)
+    icon=models.CharField(max_length=20)
+    number=models.IntegerField()
+    description_icon=models.CharField(max_length=400)
+
+
+
+    
