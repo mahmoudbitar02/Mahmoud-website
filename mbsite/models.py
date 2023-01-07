e421803183952b7810c7a86c73e4ef4c2b177aa5
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2023-01-07 23:34:04 +0100

    add skills class

--- mbsite/models.py
+++ mbsite/models.py
@@ -17,5 +17,19 @@ class About(models.Model):
     description_icon=models.CharField(max_length=400)
 
 
+SKILL_TYPE=(
+    ('HTML','HTML'),
+    ('CSS','CSS'),
+    ('JAVASCRIPT','JAVASCRIPT'),
+    ('PYTHON', 'PYTHON'),
+    ('DJANGO','DJANGO')
+
+)    
+
+class Skills(models.Model):
+    skill_description=models.CharField(max_length=500)
+    percentage=models.IntegerField()
+    typ=models.CharField(max_length=20, choices=SKILL_TYPE)
+
 
     
