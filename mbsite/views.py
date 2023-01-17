3e69cfbfd440dbcc62bdb54c2ac53771e6eadb33
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2023-01-17 22:47:41 +0100

    add datat html

--- mbsite/views.py
+++ mbsite/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .models import Mysilf
+from .models import Mysilf, About
 
 def home(request):
     mysilf=Mysilf.objects.last()
+    about=About.objects.all()
 
     return render(request, 'index.html',{
 
     'mysilf':mysilf,
+    'about':about
+
     })
